03eb410c70f9737948ff2107cbab1975d495e1f9
Thomas Bonk <thomas@meandmymac.de>
2022-06-24 12:49:15 +0200

    Initial Commit

---
+++ Sources/mdprsKit/mdprsKit.swift
@@ -0,0 +1,6 @@
+public struct mdprsKit {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/mdprsKitTests/mdprsKitTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import mdprsKit
+
+final class mdprsKitTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(mdprsKit().text, "Hello, World!")
+    }
+}
